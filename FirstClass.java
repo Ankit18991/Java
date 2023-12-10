adb762b8f94f00a05886a911aa94f527ee3a6664
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-12-10 12:22:57 +0530

    1> Our First Java program

---
+++ FirstClass.java
@@ -0,0 +1,7 @@
+class FirstClass{
+    public static void main(String[] args) {
+        // The name of the file = name of the class
+        System.out.println("Hello World");
+        // Here Sytsem is the class that we are using in the java
+    }
+}
